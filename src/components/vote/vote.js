a177e5d0eb6218525851bb085413ece85feeeb21
mellkior <38818928+mellkior@users.noreply.github.com>
2024-07-13 17:41:33 -0400

    updated monster vote to use public API

--- src/components/vote/vote.js
+++ src/components/vote/vote.js
@@ -1,41 +1,46 @@
 
 class Vote extends HTMLElement {
 
-    // Constants.
-    API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJzZXJ2aWNlX3JvbGUiLAogICAgImlzcyI6ICJzdXBhYmFzZS1kZW1vIiwKICAgICJpYXQiOiAxNjQxNzY5MjAwLAogICAgImV4cCI6IDE3OTk1MzU2MDAKfQ.DaYlNEoUrrEn2Ig7tqibS-PHK5vgusbcbo7X36XVt4Q"
-    BASE_URL = "http://localhost:8000"
-
     constructor() {
         super();
     }
 
     async connectedCallback() {
-        let res = await fetch('/components/vote/vote.html');
+        // Constants for public API.
+        const anon_api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZudm13b2RnenlibmNsYWFsa3h4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA5MDIyMjQsImV4cCI6MjAzNjQ3ODIyNH0.5fPrBmMp7MbEhl62rX3IxKwVKOvrFiXg3fipe9fRLwA";
+        const api_base_url = "https://vnvmwodgzybnclaalkxx.supabase.co";
+        // TODO: default to current year.
+        const year = 2023;
 
+        // Fetch the HTML for the custom component.
+        const res = await fetch('/components/vote/vote.html');
+        // Insert the HTML into the DOM.
         this.innerHTML = await res.text();
-        await this.getData();
-        this.registerHandlers();
+        // Fetch the available monsters from the public API.
+        await this.getData(api_base_url, anon_api_key, year);
+        // Wire up the form to POST data to the API on submit.
+        this.registerHandlers(api_base_url, anon_api_key);
     }
 
-    registerHandlers() {
-        var form = document.getElementById('monster-vote-form');
+    registerHandlers(base_url, anon_key) {
+        const form = document.getElementById('monster-vote-form');
 
         form.addEventListener("submit", (event) => {
             event.preventDefault();
-            this.submitForm();
+            this.submitForm(base_url, anon_key);
         })
     }
 
-    async getData() {
-        const url = `${this.BASE_URL}/rest/v1/monsters`;
+    async getData(base_url, anon_key, year) {
+        const url = `${base_url}/rest/v1/monsters?season=eq.${year}`;
 
         const monsterOptions = document.getElementById('monster-options');
 
         try {
             const response = await fetch(url, {
                 headers: {
-                    "apiKey": this.API_KEY,
-                    "Authorization": `Bearer ${this.API_KEY}`
+                    "apiKey": anon_key,
+                    "Authorization": `Bearer ${anon_key}`
                 }
             });
 
@@ -44,6 +49,7 @@ class Vote extends HTMLElement {
             }
 
             const json = await response.json();
+            // TODO: remove logging.
             console.log(json);
 
             // Add the dynamically generated monster options to the div.
@@ -59,7 +65,7 @@ class Vote extends HTMLElement {
 
     // Note: nothing is escaped or sanitized, input is assumed trusted.
     buildMonsterOption(json) {
-        var html = `
+        const html = `
         <div class="w-48 lg:w-60 relative place-self-center justify-self-center">
             <input id="monster-${json.id}" name="favorite_monster" value="${json.id}" type="radio" class="peer hidden">
             <label for="monster-${json.id}"
@@ -79,23 +85,24 @@ class Vote extends HTMLElement {
         return html;
     }
 
-    async submitForm() {
-        const url = `${this.BASE_URL}/rest/v1/votes`;
+    async submitForm(base_url, anon_key) {
+        const url = `${base_url}/rest/v1/votes`;
 
         // Get form data.
         const form = document.getElementById('monster-vote-form');
         const formData = new FormData(form);
-        var object = {};
+        const object = {};
         formData.forEach((value, key) => object[key] = value);
-        var formJson = object
+        const formJson = object
+        // TODO: remove logging.
         console.log(formJson)
 
         try {
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
-                    "apiKey": this.API_KEY,
-                    "Authorization": `Bearer ${this.API_KEY}`,
+                    "apiKey": anon_key,
+                    "Authorization": `Bearer ${anon_key}`,
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify(formJson)
@@ -116,5 +123,5 @@ class Vote extends HTMLElement {
     }
 }
 
+// Register the custom component to be used in HTML with the <stellmar-vote> tag.
 customElements.define('stellmar-vote', Vote);
-
